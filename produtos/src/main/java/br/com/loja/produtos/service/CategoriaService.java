88b8e896b4a911dd0b577f34f804ae5b31d6751c
unknown <leoneduardoperez@gmail.com>
2021-09-17 16:48:28 -0300

    amqp

--- produtos/src/main/java/br/com/loja/produtos/service/CategoriaService.java
+++ produtos/src/main/java/br/com/loja/produtos/service/CategoriaService.java
@@ -2,24 +2,39 @@ package br.com.loja.produtos.service;
 
 import br.com.loja.produtos.domain.Categoria;
 import br.com.loja.produtos.repository.CategoriaRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CategoriaService {
+
 
     @Autowired
     CategoriaRepository categoriaRepository;
 
+    @Autowired
+    ObjectMapper objectMapper;
 
     public Categoria save(Categoria categoria) {
         return categoriaRepository.save(categoria);
     }
 
-    public Page<Categoria> getAll(Pageable pageable){
+    public Page<Categoria> getAll(Pageable pageable) {
         return categoriaRepository.findAll(pageable);
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void receiveMessage(@Payload Categoria categoria) throws JsonProcessingException {
+        save(categoria);
+        log.info("Mensagem persistida");
     }
 
 }
